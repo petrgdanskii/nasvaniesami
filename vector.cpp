b96c023d3dfb0cf69148948e313c9f385e86dcd3
petrgdanskii <78430757+petrgdanskii@users.noreply.github.com>
2022-12-20 01:27:10 +0300

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 #include "vector.h"
+#include <cmath>
 
 vector :: vector (int v_size, std::string v_id = "unknown"){
     this->v_size = v_size;
@@ -23,4 +24,12 @@ void vector :: set_data (int v_size, double* v_data){
     for (int i = 0; i < v_size; i++){
         this->v_data[i]=v_data[i];
     };
+};
+
+double vector :: norm2() {
+	double n = 0;
+	for (int i = 0 ; i < this->v_size; i++){
+		n += this->v_data[i] * this->v_data[i];
+	}
+	return sqrt(n);
 };
